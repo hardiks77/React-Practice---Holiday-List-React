a0a159c4e409a6a6880239f88630a2efc8d05d34
Hardik Sharma <33150286+hardiks77@users.noreply.github.com>
2021-08-14 14:11:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,7 @@ class App extends Component {
         <ol>
           {
             this.cityList.filter(city => city.country == "India").map((cityName, index) => (
-                <li key={index + 1}>{cityName.name}</li>
+                <li key={["location", (index + 1)]}>{cityName.name}</li>
             ))
           }
         </ol>
